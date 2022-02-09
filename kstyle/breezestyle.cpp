930d23d4087a546a011eb5fa242f395df2dbc290
Ingo Klöcker <dev@ingo-kloecker.de>
2022-02-09 17:55:58 +0100

    Fix "Blue Ocean" styling of QCommandLinkButtons
    As for all other buttons QPalette::HighlightedText must not be used
    anymore for the text of focussed buttons.

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -1470,8 +1470,6 @@ namespace Breeze
             // state
             const State& state( option.state );
             const bool enabled( state & State_Enabled );
-            bool mouseOver( enabled && ( state & State_MouseOver ) );
-            bool hasFocus( enabled && ( state & State_HasFocus ) );
 
             // icon
             if( !button->icon().isNull() )
@@ -1490,7 +1488,7 @@ namespace Breeze
 
             // text rect
             QRect textRect( offset, QSize( button->size().width() - offset.x() - margin, button->size().height() - 2*margin ) );
-            const QPalette::ColorRole textRole = (enabled && hasFocus && !mouseOver && !isFlat ) ? QPalette::HighlightedText : QPalette::ButtonText;
+            const QPalette::ColorRole textRole = QPalette::ButtonText;
             if( !button->text().isEmpty() )
             {
 
