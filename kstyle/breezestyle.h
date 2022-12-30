1eb437101d4a853ceaaa049989d1f9501832eefb
Noah Davis <noahadvs@gmail.com>
2022-12-30 11:45:29 -0500

    Change menu section style to label and inline separator
    Also make separator width the same as menu item content to keep the
    margins around the sections and separators similar on both sides.

--- kstyle/breezestyle.h
+++ kstyle/breezestyle.h
@@ -344,9 +344,6 @@ private:
     //* spinbox arrows
     void renderSpinBoxArrow(const SubControl &, const QStyleOptionSpinBox *, QPainter *, const QWidget *) const;
 
-    //* menu title
-    void renderMenuTitle(const QStyleOptionToolButton *, QPainter *, const QWidget *) const;
-
     //* return dial angle based on option and value
     qreal dialAngle(const QStyleOptionSlider *, int) const;
 
@@ -492,9 +489,6 @@ private:
 
     //* return true if icons should be shown on buttons
     bool showIconsOnPushButtons() const;
-
-    //* return true if passed widget is a menu title (KMenu::addTitle)
-    bool isMenuTitle(const QWidget *) const;
 
     //* return true if passed widget is a menu title (KMenu::addTitle)
     bool hasAlteredBackground(const QWidget *) const;
