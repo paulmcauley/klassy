1258b4c9e39e97d6d7fd18c10bae1a116dadf0e3
Akseli Lahtinen <akselmo@akselmo.dev>
2024-04-04 07:36:41 +0000

    Radiobuttons and checkboxes: Use button color instead of base color for background
    Due to bugfix in https://invent.kde.org/frameworks/qqc2-desktop-style/-/merge_requests/389 we need to unify the colors here
    to be the same across QtWidgets and QtQuick apps.
    
    It also makes sense to have all the interactable button elements to have same colors.
    The value is no more a magically modified value either, which will help with coherency and people can modify them to suit their needs.

--- kstyle/breezehelper.cpp
+++ kstyle/breezehelper.cpp
@@ -876,7 +876,7 @@ void Helper::renderCheckBoxBackground(QPainter *painter,
 
     switch (state) {
     case CheckOff:
-        painter->setBrush(palette.base().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
+        painter->setBrush(palette.button().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
         painter->drawRoundedRect(frameRect, radius, radius);
         break;
 
@@ -887,7 +887,7 @@ void Helper::renderCheckBoxBackground(QPainter *painter,
         break;
 
     case CheckAnimated:
-        painter->setBrush(palette.base().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
+        painter->setBrush(palette.button().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
         painter->drawRoundedRect(frameRect, radius, radius);
         painter->setBrush(transparent);
         painter->setOpacity(animation);
@@ -1034,7 +1034,7 @@ void Helper::renderRadioButtonBackground(QPainter *painter,
 
     switch (state) {
     case RadioOff:
-        painter->setBrush(palette.base().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
+        painter->setBrush(palette.button().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
         painter->drawEllipse(frameRect);
         break;
     case RadioOn:
@@ -1042,7 +1042,7 @@ void Helper::renderRadioButtonBackground(QPainter *painter,
         painter->drawEllipse(frameRect);
         break;
     case RadioAnimated:
-        painter->setBrush(palette.base().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
+        painter->setBrush(palette.button().color().darker(sunken ? radioCheckSunkenDarkeningFactor : 100));
         painter->drawEllipse(frameRect);
         painter->setBrush(transparent);
         painter->setOpacity(animation);
