49d76dd6b42a99bb8b8ad6feb640a1f3f1a9bda9
Carl Schwan <carl@carlschwan.eu>
2024-08-15 22:43:42 +0200

    Simplify eventFilterDockWidget
    The condition can be moved to the begining of the method

--- kstyle/breezestyle.cpp
+++ kstyle/breezestyle.cpp
@@ -1993,7 +1993,7 @@ void Style::drawToolsAreaBackgroundAndSeparator(QPainter *painter, const QWidget
 //____________________________________________________________________________
 bool Style::eventFilterDockWidget(QDockWidget *dockWidget, QEvent *event)
 {
-    if (event->type() == QEvent::Paint) {
+    if (event->type() == QEvent::Paint && dockWidget->isFloating()) {
         // create painter and clip
         QPainter painter(dockWidget);
         QPaintEvent *paintEvent = static_cast<QPaintEvent *>(event);
@@ -2008,9 +2008,7 @@ bool Style::eventFilterDockWidget(QDockWidget *dockWidget, QEvent *event)
         const auto rect(dockWidget->rect());
 
         // render
-        if (dockWidget->isFloating()) {
-            _helper->renderMenuFrame(&painter, rect, background, outline, false);
-        }
+        _helper->renderMenuFrame(&painter, rect, background, outline, false);
     }
 
     return false;
