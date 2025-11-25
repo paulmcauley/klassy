4b512fa7800f1d20585d00b7561c63d89703289e
Kunda Ki <luzpaz@pm.me>
2025-11-25 01:06:23 +0000

    kstyle: fix source typo isDragable->isDraggable

--- kstyle/breezewindowmanager.cpp
+++ kstyle/breezewindowmanager.cpp
@@ -151,7 +151,7 @@ void WindowManager::initialize()
 //_____________________________________________________________
 void WindowManager::registerWidget(QWidget *widget)
 {
-    if (isBlackListed(widget) || isDragable(widget) || widget->inherits("QQuickWidget")) {
+    if (isBlackListed(widget) || isDraggable(widget) || widget->inherits("QQuickWidget")) {
         /*
         install filter for draggable widgets.
         also install filter for blacklisted widgets
@@ -442,7 +442,7 @@ bool WindowManager::mouseReleaseEvent(QObject *object, QEvent *event)
 }
 
 //_____________________________________________________________
-bool WindowManager::isDragable(QWidget *widget)
+bool WindowManager::isDraggable(QWidget *widget)
 {
     // check widget
     if (!widget) {
@@ -570,7 +570,7 @@ bool WindowManager::canDrag(QWidget *widget)
         return false;
     }
 
-    // assume isDragable widget is already passed
+    // assume isDraggable widget is already passed
     // check some special cases where drag should not be effective
 
     // check mouse grabber
