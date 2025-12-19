eeb7563f22febd127ee5289ea3f8f2f7d2c51c7f
Paul A McAuley <kde@paulmcauley.com>
2025-12-19 15:06:47 +0000

    Update Global Themes to use Kairn

--- look-and-feel/org.kde.klassykisweetdarkleftpanel.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++
@@ -1,5 +0,0 @@
-loadTemplate("org.kde.klassy.plasma.desktop.leftPanel");
-
-const kwinConfig = ConfigFile('kwinrc');
-kwinConfig.group = 'Effect-overview';
-kwinConfig.writeEntry('BorderActivate', '5'); //overview all desktops bottom-left
