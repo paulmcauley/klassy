832fa6c2f57313a751bdfb7d23b2ee3855107b35
Paul A McAuley <kde@paulmcauley.com>
2026-01-06 00:47:38 +0000

    Remove Kopal global theme and don't install colours

--- look-and-feel/org.kde.klassykopallightbottompanel.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++
@@ -1,9 +0,0 @@
-loadTemplate("org.kde.klassy.plasma.desktop.bottomPanel");
-
-const kwinConfig = ConfigFile('kwinrc');
-
-kwinConfig.group = 'Effect-overview';
-kwinConfig.writeEntry('BorderActivate', '9'); //disable overview
-
-kwinConfig.group = 'Effect-windowview';
-kwinConfig.writeEntry('BorderActivateAll', '3'); //overview all desktops bottom-right
