de71f0877d5054bdb4562a326966a18e2c82ecbb
Paul A McAuley <kde@paulmcauley.com>
2026-01-17 00:16:30 +0000

    BreezeStyle: Read outline contrast from kdeglobals
    Read the `frameContrast` value from `KColorScheme`. This also updates
    it whenever it's changed.
    
    Contrast applied to unfocused scrollbars only.
    
    No change made to KDecoration as Klassy outline contrast is calculated
    differently to Breeze by using the titlebar font.
    
    See issue https://invent.kde.org/teams/vdg/issues/-/issues/108

--- kstyle/breezemetrics.h
+++ kstyle/breezemetrics.h
@@ -146,11 +146,6 @@ static constexpr int Splitter_SplitterWidth = 1;
 
 // shadow dimensions
 static constexpr int Shadow_Overlap = 2;
-
-// frame intensities (called bias in KColorUtilities::Mix)
-// Keep this value in sync with Kirigami  PlatformTheme::frameContrast()
-// https://invent.kde.org/frameworks/kirigami/-/blob/master/src/platform/platformtheme.cpp?ref_type=heads#L701
-static constexpr qreal Bias_Default = 0.20;
 }
 
 }
