11b5446b349477b41c4cc86be5f70a6ac56e6f7c
Remilia <noreply@remilia.dev>
2026-08-04 20:41:54 -0600

    Add horizontal margin setting for integrated menu

--- kdecoration/breezedecoration.cpp
+++ kdecoration/breezedecoration.cpp
@@ -1440,7 +1440,7 @@ void Decoration::updateButtonsGeometry()
                 m_integratedMenuButtons->setPos(availableRect.topLeft());
             }
 
-            m_integratedMenuButtons->setSpacing(0);
+            m_integratedMenuButtons->setSpacing(m_internalSettings->menuButtonHorzMargin());
         }
     }
 
