16f1545629b5183db329242225f318ef7f995dd5
Remilia <noreply@remilia.dev>
2026-08-13 21:20:40 -0600

    Fix integrated buttons getting stuck unison hovered

--- kdecoration/breezedecoration.cpp
+++ kdecoration/breezedecoration.cpp
@@ -2128,6 +2128,7 @@ void Decoration::hoverLeaveEvent(QHoverEvent *event)
 
     if (m_integratedMenuButtons) {
         m_integratedMenuButtons->setHovered(false);
+        m_integratedMenuButtons->setUnisonHovered(false);
         m_integratedMenuButtons->updateShowing();
     }
     KDecoration3::Decoration::hoverLeaveEvent(event);
